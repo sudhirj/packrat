244a743f8f796a0b98f17fc0fdd6f30dc3b36376
Sudhir Jonathan <sudhir.j@gmail.com>
2016-08-11 00:02:03 +0530

    Used explicit env vars

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-	sess, err := session.NewSession()
-	if err != nil {
-		log.Fatal("Could not start AWS session")
-	}
-	s3Svc := s3.New(sess)
-	uploader := s3manager.NewUploaderWithClient(s3Svc)
+	uploader := s3manager.NewUploader(session.New(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_REGION")),
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("AWS_ACCESS_KEY_ID"),
+			os.Getenv("AWS_SECRET_ACCESS_KEY"),
+			"",
+		)}))
 
 	http.ListenAndServe(":"+port, http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
 			pathParts := strings.Split(r.URL.Path, "/")
 			bucket, remainingParts := pathParts[0], pathParts[1:]
 			id := r.Header.Get("Logplex-Frame-Id")
@@ -33,5 +34,4 @@ func main() {
 				Body:   r.Body,
 			})
 		}))
-
 }
